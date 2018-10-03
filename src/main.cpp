ffc231674b8dda722b879902e1e4c19c5feb871d
Andrii Bilynskyi <Andrii.Bilynski@opensynergy.com>
2018-10-03 13:21:04 +0300

    Added ping multiple hosts, refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -20,16 +20,6 @@ sudo apt-get install libconfuse-dev
 
 static const char config_filename[] = "/etc/con-check/con-check.conf";
 
-static char * host                  = NULL;      // host to ping
-static char * device                = NULL;      // interface to send pings
-static char * online_cmd            = NULL;      // on on-line script
-static char * offline_cmd           = NULL;      // on off-line script
-static unsigned long int up_cnt     = 10;        // consistent success pings to set connection up
-static unsigned long int down_cnt   = 10;        // consistent unsuccess pings to set connection down
-static unsigned long int start_cnt  = 10;        // check initial state pings count
-static unsigned long int interval   = 2000;      // interval between pings in ms
-static unsigned long int ptimeout   = 1000;      // ping timeout in ms
-
 static volatile bool terminate = false;
 
 void sig_handler(int signum)
@@ -38,17 +28,17 @@ void sig_handler(int signum)
     terminate = true;
 }
 
-void change_sate(bool is_online)
+void change_sate(cfg_t * cfg, bool is_online)
 {
     if(is_online)
     {
         trace_log_printf(LOG_DEBUG, "transition to on-line");
-        system(online_cmd);
+        system(cfg_getstr(cfg, "online_cmd"));
     }
     else
     {
         trace_log_printf(LOG_DEBUG, "transition to off-line");
-        system(offline_cmd);
+        system(cfg_getstr(cfg, "offline_cmd"));
     }
 }
 
@@ -58,33 +48,43 @@ int main()
     trace_log_printf(LOG_INFO, "application started");
 
     cfg_opt_t opts[] = {
-        CFG_SIMPLE_STR("host", &host),
-        CFG_SIMPLE_STR("device", &device),
-        CFG_SIMPLE_STR("online_cmd", &online_cmd),
-        CFG_SIMPLE_STR("offline_cmd", &offline_cmd),
-        CFG_SIMPLE_INT("up_cnt", &up_cnt),
-        CFG_SIMPLE_INT("down_cnt", &down_cnt),
-        CFG_SIMPLE_INT("start_cnt", &start_cnt),
-        CFG_SIMPLE_INT("interval", &interval),
-        CFG_SIMPLE_INT("ptimeout", &ptimeout),
+        CFG_STR_LIST("hosts", "{}", CFGF_NONE),
+        CFG_STR("device", "", CFGF_NONE),
+        CFG_STR("online_cmd", "", CFGF_NONE),
+        CFG_STR("offline_cmd", "", CFGF_NONE),
+        CFG_INT("up_cnt", 10, CFGF_NONE),
+        CFG_INT("down_cnt", 10, CFGF_NONE),
+        CFG_INT("start_cnt", 10, CFGF_NONE),
+        CFG_INT("interval", 2000, CFGF_NONE),
+        CFG_INT("ptimeout", 1000, CFGF_NONE),
         CFG_END()
     };
 
     cfg_t * cfg = cfg_init(opts, 0);
     (void)cfg_parse(cfg, config_filename);
-
-    if(host && device && online_cmd && offline_cmd && up_cnt && down_cnt && start_cnt && interval && ptimeout)
+    for(unsigned int i = 0; i < cfg_size(cfg, "hosts"); i++)
     {
-        trace_log_printf(LOG_DEBUG, "host: %s", host);
-        trace_log_printf(LOG_DEBUG, "device: %s", device);
-        trace_log_printf(LOG_DEBUG, "online_cmd: %s", online_cmd);
-        trace_log_printf(LOG_DEBUG, "offline_cmd: %s", offline_cmd);
-        trace_log_printf(LOG_DEBUG, "up_cnt: %u", up_cnt);
-        trace_log_printf(LOG_DEBUG, "down_cnt: %u", down_cnt);
-        trace_log_printf(LOG_DEBUG, "start_cnt: %u", start_cnt);
-        trace_log_printf(LOG_DEBUG, "interval: %u", interval);
-        trace_log_printf(LOG_DEBUG, "ptimeout: %u", ptimeout);
+        trace_log_printf(LOG_DEBUG, "host[%d]: %s", i, cfg_getnstr(cfg, "hosts", i));
+    }
+    trace_log_printf(LOG_DEBUG, "device: %s", cfg_getstr(cfg, "device"));
+    trace_log_printf(LOG_DEBUG, "online_cmd: %s", cfg_getstr(cfg, "online_cmd"));
+    trace_log_printf(LOG_DEBUG, "offline_cmd: %s", cfg_getstr(cfg, "offline_cmd"));
+    trace_log_printf(LOG_DEBUG, "up_cnt: %u", cfg_getint(cfg, "up_cnt"));
+    trace_log_printf(LOG_DEBUG, "down_cnt: %u", cfg_getint(cfg, "down_cnt"));
+    trace_log_printf(LOG_DEBUG, "start_cnt: %u", cfg_getint(cfg, "start_cnt"));
+    trace_log_printf(LOG_DEBUG, "interval: %u", cfg_getint(cfg, "interval"));
+    trace_log_printf(LOG_DEBUG, "ptimeout: %u", cfg_getint(cfg, "ptimeout"));
 
+    if( cfg_size(cfg, "hosts")                                                     &&
+        strcmp(cfg_getstr(cfg, "device"), "")                                      &&
+        strcmp(cfg_getstr(cfg, "online_cmd"), "")                                  &&
+        strcmp(cfg_getstr(cfg, "offline_cmd"), "")                                 &&
+        cfg_getint(cfg, "up_cnt")    > 0 &&  cfg_getint(cfg, "up_cnt")    < 100    &&
+        cfg_getint(cfg, "down_cnt")  > 0 &&  cfg_getint(cfg, "down_cnt")  < 100    &&
+        cfg_getint(cfg, "start_cnt") > 0 &&  cfg_getint(cfg, "start_cnt") < 100    &&
+        cfg_getint(cfg, "interval")  > 0 &&  cfg_getint(cfg, "interval")  < 100000 &&
+        cfg_getint(cfg, "ptimeout")  > 0 &&  cfg_getint(cfg, "ptimeout")  < 100000 )
+    {
         struct sigaction act;
         memset(&act, 0, sizeof(act));
         act.sa_handler = sig_handler;
@@ -95,78 +95,76 @@ int main()
         if(ping)
         {
             trace_log_printf(LOG_INFO, "ping object created");
-            double timeout = (double)ptimeout / 1000;
+            double timeout = (double)cfg_getint(cfg, "ptimeout") / 1000;
             if(!ping_setopt(ping, PING_OPT_TIMEOUT, &timeout))
             {
-                if(!ping_setopt(ping, PING_OPT_DEVICE, (void*)device))
+                if(!ping_setopt(ping, PING_OPT_DEVICE, cfg_getstr(cfg, "device")))
                 {
-                    if(!ping_host_add(ping, host))
+                    for(unsigned int i = 0; i < cfg_size(cfg, "hosts"); i++)
                     {
-                        trace_log_printf(LOG_INFO, "ping host added");
-                        unsigned long int success = 0, unsuccess = 0;
-                        for(unsigned long  i = 0; !terminate && i < start_cnt; i++)
+                        if(ping_host_add(ping, cfg_getnstr(cfg, "hosts", i)))
                         {
-                            if(ping_send(ping) > 0)
-                            {
-                                success++;
-                            }
-                            else
-                            {
-                                unsuccess++;
-                            }
-                        }
-
-                        bool on_line = false;
-                        if(success > unsuccess)
-                        {
-                            on_line = true;
-                        }
-                        change_sate(on_line);
-                        unsigned long int conter = 0;
-                        while(!terminate)
-                        {
-                            int result = ping_send(ping);
-                            if(on_line)
-                            {
-                                if(result >= 0)
-                                {
-                                    conter = 0;
-                                }
-                                else
-                                {
-                                    conter++;
-                                }
-                                if(conter >= down_cnt)
-                                {
-                                    on_line = false;
-                                    conter  = 0;
-                                    change_sate(on_line);
-                                }
-                            }
-                            else
-                            {
-                                if(result >= 0)
-                                {
-                                    conter++;
-                                }
-                                else
-                                {
-                                    conter = 0;
-                                }
-                                if(conter >= up_cnt)
-                                {
-                                    on_line = true;
-                                    conter  = 0;
-                                    change_sate(on_line);
-                                }
-                            }
-                            usleep(interval * 1000u);
+                            trace_log_printf(LOG_ERROR, "can't add host %s : %s", cfg_getnstr(cfg, "hosts", i), ping_get_error(ping));
                         }
                     }
-                    else
+
+                    unsigned int success = 0, unsuccess = 0;
+                    for(unsigned int  i = 0; !terminate && i < cfg_getint(cfg, "start_cnt"); i++)
                     {
-                        trace_log_printf(LOG_ERROR, "can't add host %s : %s", host, ping_get_error(ping));
-                        change_sate(false);
+                        if(ping_send(ping) > 0)
+                        {
+                            success++;
+                        }
+                        else
+                        {
+                            unsuccess++;
+                        }
+                        usleep(cfg_getint(cfg, "interval") * 1000u);
+                    }
+
+                    bool on_line = (success > unsuccess);
+                        
+                    change_sate(cfg, on_line);
+                    unsigned int conter = 0;
+                    while(!terminate)
+                    {
+                        int result = ping_send(ping);
+                        //trace_log_printf(LOG_INFO, "ping response count %d", result);
+                        if(on_line)
+                        {
+                            if(result > 0)
+                            {
+                                conter = 0;
+                            }
+                            else
+                            {
+                                conter++;
+                            }
+                            if(conter >= (unsigned)cfg_getint(cfg, "down_cnt"))
+                            {
+                                on_line = false;
+                                conter  = 0;
+                                change_sate(cfg, on_line);
+                            }
+                        }
+                        else
+                        {
+                            if(result > 0)
+                            {
+                                conter++;
+                            }
+                            else
+                            {
+                                conter = 0;
+                            }
+                            if(conter >= (unsigned)cfg_getint(cfg, "up_cnt"))
+                            {
+                                on_line = true;
+                                conter  = 0;
+                                change_sate(cfg, on_line);
+                            }
+                        }
+                        usleep(cfg_getint(cfg, "interval") * 1000u);
                     }
                 }
                 else
