f911b357a4f7861ed2e4333b7f108014b442746f
Andrii Bilynskyi <Andrii.Bilynski@opensynergy.com>
2018-10-29 18:20:26 +0200

    Add usage custom config file

--- src/main.cpp
+++ src/main.cpp
@@ -48,156 +48,169 @@ void change_sate(cfg_t * cfg, bool is_online)
     }
 }
 
-int main()
+int main(int argc, char *argv[])
 {
     trace_log_setlevel(LOG_INFO);
     trace_log_printf(LOG_INFO, "application started");
 
-    cfg_opt_t opts[] = {
-        CFG_STR_LIST("hosts", "{}", CFGF_NONE),
-        CFG_STR("device", "", CFGF_NONE),
-        CFG_STR("online_cmd", "", CFGF_NONE),
-        CFG_STR("offline_cmd", "", CFGF_NONE),
-        CFG_INT("up_cnt", 10, CFGF_NONE),
-        CFG_INT("down_cnt", 10, CFGF_NONE),
-        CFG_INT("start_cnt", 10, CFGF_NONE),
-        CFG_INT("interval", 2000, CFGF_NONE),
-        CFG_INT("ptimeout", 1000, CFGF_NONE),
-        CFG_END()
-    };
-
-    cfg_t * cfg = cfg_init(opts, 0);
-    cfg_set_error_function(cfg, print_cfg_error);
-    (void)cfg_parse(cfg, config_filename);
-    for(unsigned int i = 0; i < cfg_size(cfg, "hosts"); i++)
+    if(argc <= 2)
     {
-        trace_log_printf(LOG_DEBUG, "host[%d]: %s", i, cfg_getnstr(cfg, "hosts", i));
-    }
-    trace_log_printf(LOG_DEBUG, "device: %s", cfg_getstr(cfg, "device"));
-    trace_log_printf(LOG_DEBUG, "online_cmd: %s", cfg_getstr(cfg, "online_cmd"));
-    trace_log_printf(LOG_DEBUG, "offline_cmd: %s", cfg_getstr(cfg, "offline_cmd"));
-    trace_log_printf(LOG_DEBUG, "up_cnt: %u", cfg_getint(cfg, "up_cnt"));
-    trace_log_printf(LOG_DEBUG, "down_cnt: %u", cfg_getint(cfg, "down_cnt"));
-    trace_log_printf(LOG_DEBUG, "start_cnt: %u", cfg_getint(cfg, "start_cnt"));
-    trace_log_printf(LOG_DEBUG, "interval: %u", cfg_getint(cfg, "interval"));
-    trace_log_printf(LOG_DEBUG, "ptimeout: %u", cfg_getint(cfg, "ptimeout"));
-
-    if( cfg_size(cfg, "hosts")                                                     &&
-        strcmp(cfg_getstr(cfg, "device"), "")                                      &&
-        strcmp(cfg_getstr(cfg, "online_cmd"), "")                                  &&
-        strcmp(cfg_getstr(cfg, "offline_cmd"), "")                                 &&
-        cfg_getint(cfg, "up_cnt")    > 0 &&  cfg_getint(cfg, "up_cnt")    < 100    &&
-        cfg_getint(cfg, "down_cnt")  > 0 &&  cfg_getint(cfg, "down_cnt")  < 100    &&
-        cfg_getint(cfg, "start_cnt") > 0 &&  cfg_getint(cfg, "start_cnt") < 100    &&
-        cfg_getint(cfg, "interval")  > 0 &&  cfg_getint(cfg, "interval")  < 100000 &&
-        cfg_getint(cfg, "ptimeout")  > 0 &&  cfg_getint(cfg, "ptimeout")  < 100000 )
-    {
-        struct sigaction act;
-        memset(&act, 0, sizeof(act));
-        act.sa_handler = sig_handler;
-        sigemptyset(&act.sa_mask);
-        (void)sigaction(SIGINT, &act, NULL);
-
-        pingobj_t * ping = ping_construct();
-        if(ping)
+        const char * cfg_fname = config_filename;
+        if(argc == 2)
         {
-            trace_log_printf(LOG_INFO, "ping object created");
-            double timeout = (double)cfg_getint(cfg, "ptimeout") / 1000;
-            if(!ping_setopt(ping, PING_OPT_TIMEOUT, &timeout))
+            cfg_fname = argv[1];
+        }
+        trace_log_printf(LOG_DEBUG, "using config file %s", cfg_fname);
+        cfg_opt_t opts[] = {
+            CFG_STR_LIST("hosts", "{}", CFGF_NONE),
+            CFG_STR("device", "", CFGF_NONE),
+            CFG_STR("online_cmd", "", CFGF_NONE),
+            CFG_STR("offline_cmd", "", CFGF_NONE),
+            CFG_INT("up_cnt", 10, CFGF_NONE),
+            CFG_INT("down_cnt", 10, CFGF_NONE),
+            CFG_INT("start_cnt", 10, CFGF_NONE),
+            CFG_INT("interval", 2000, CFGF_NONE),
+            CFG_INT("ptimeout", 1000, CFGF_NONE),
+            CFG_END()
+        };
+
+        cfg_t * cfg = cfg_init(opts, 0);
+        cfg_set_error_function(cfg, print_cfg_error);
+        (void)cfg_parse(cfg, cfg_fname);
+        for(unsigned int i = 0; i < cfg_size(cfg, "hosts"); i++)
+        {
+            trace_log_printf(LOG_DEBUG, "host[%d]: %s", i, cfg_getnstr(cfg, "hosts", i));
+        }
+        trace_log_printf(LOG_DEBUG, "device: %s", cfg_getstr(cfg, "device"));
+        trace_log_printf(LOG_DEBUG, "online_cmd: %s", cfg_getstr(cfg, "online_cmd"));
+        trace_log_printf(LOG_DEBUG, "offline_cmd: %s", cfg_getstr(cfg, "offline_cmd"));
+        trace_log_printf(LOG_DEBUG, "up_cnt: %u", cfg_getint(cfg, "up_cnt"));
+        trace_log_printf(LOG_DEBUG, "down_cnt: %u", cfg_getint(cfg, "down_cnt"));
+        trace_log_printf(LOG_DEBUG, "start_cnt: %u", cfg_getint(cfg, "start_cnt"));
+        trace_log_printf(LOG_DEBUG, "interval: %u", cfg_getint(cfg, "interval"));
+        trace_log_printf(LOG_DEBUG, "ptimeout: %u", cfg_getint(cfg, "ptimeout"));
+
+        if( cfg_size(cfg, "hosts")                                                     &&
+            strcmp(cfg_getstr(cfg, "device"), "")                                      &&
+            strcmp(cfg_getstr(cfg, "online_cmd"), "")                                  &&
+            strcmp(cfg_getstr(cfg, "offline_cmd"), "")                                 &&
+            cfg_getint(cfg, "up_cnt")    > 0 &&  cfg_getint(cfg, "up_cnt")    < 100    &&
+            cfg_getint(cfg, "down_cnt")  > 0 &&  cfg_getint(cfg, "down_cnt")  < 100    &&
+            cfg_getint(cfg, "start_cnt") > 0 &&  cfg_getint(cfg, "start_cnt") < 100    &&
+            cfg_getint(cfg, "interval")  > 0 &&  cfg_getint(cfg, "interval")  < 100000 &&
+            cfg_getint(cfg, "ptimeout")  > 0 &&  cfg_getint(cfg, "ptimeout")  < 100000 )
+        {
+            struct sigaction act;
+            memset(&act, 0, sizeof(act));
+            act.sa_handler = sig_handler;
+            sigemptyset(&act.sa_mask);
+            (void)sigaction(SIGINT, &act, NULL);
+
+            pingobj_t * ping = ping_construct();
+            if(ping)
             {
-                if(!ping_setopt(ping, PING_OPT_DEVICE, cfg_getstr(cfg, "device")))
+                trace_log_printf(LOG_INFO, "ping object created");
+                double timeout = (double)cfg_getint(cfg, "ptimeout") / 1000;
+                if(!ping_setopt(ping, PING_OPT_TIMEOUT, &timeout))
                 {
-                    for(unsigned int i = 0; i < cfg_size(cfg, "hosts"); i++)
+                    if(!ping_setopt(ping, PING_OPT_DEVICE, cfg_getstr(cfg, "device")))
                     {
-                        if(ping_host_add(ping, cfg_getnstr(cfg, "hosts", i)))
+                        for(unsigned int i = 0; i < cfg_size(cfg, "hosts"); i++)
                         {
-                            trace_log_printf(LOG_ERROR, "can't add host %s : %s", cfg_getnstr(cfg, "hosts", i), ping_get_error(ping));
-                        }
-                    }
-
-                    unsigned int success = 0, unsuccess = 0;
-                    for(unsigned int  i = 0; !terminate && i < cfg_getint(cfg, "start_cnt"); i++)
-                    {
-                        if(ping_send(ping) > 0)
-                        {
-                            success++;
-                        }
-                        else
-                        {
-                            unsuccess++;
-                        }
-                        usleep(cfg_getint(cfg, "interval") * 1000u);
-                    }
-
-                    bool on_line = (success > unsuccess);
-                    if(!terminate)
-                    {
-                        change_sate(cfg, on_line);
-                    }
-                    unsigned int conter = 0;
-                    while(!terminate)
-                    {
-                        int result = ping_send(ping);
-                        //trace_log_printf(LOG_INFO, "ping response count %d", result);
-                        if(on_line)
-                        {
-                            if(result > 0)
+                            if(ping_host_add(ping, cfg_getnstr(cfg, "hosts", i)))
                             {
-                                conter = 0;
+                                trace_log_printf(LOG_ERROR, "can't add host %s : %s", cfg_getnstr(cfg, "hosts", i), ping_get_error(ping));
+                            }
+                        }
+
+                        unsigned int success = 0, unsuccess = 0;
+                        for(unsigned int  i = 0; !terminate && i < cfg_getint(cfg, "start_cnt"); i++)
+                        {
+                            if(ping_send(ping) > 0)
+                            {
+                                success++;
                             }
                             else
                             {
-                                conter++;
+                                unsuccess++;
                             }
-                            if(conter >= (unsigned)cfg_getint(cfg, "down_cnt"))
-                            {
-                                on_line = false;
-                                conter  = 0;
-                                change_sate(cfg, on_line);
-                            }
+                            usleep(cfg_getint(cfg, "interval") * 1000u);
                         }
-                        else
+
+                        bool on_line = (success > unsuccess);
+                        if(!terminate)
                         {
-                            if(result > 0)
+                            change_sate(cfg, on_line);
+                        }
+                        unsigned int conter = 0;
+                        while(!terminate)
+                        {
+                            int result = ping_send(ping);
+                            //trace_log_printf(LOG_INFO, "ping response count %d", result);
+                            if(on_line)
                             {
-                                conter++;
+                                if(result > 0)
+                                {
+                                    conter = 0;
+                                }
+                                else
+                                {
+                                    conter++;
+                                }
+                                if(conter >= (unsigned)cfg_getint(cfg, "down_cnt"))
+                                {
+                                    on_line = false;
+                                    conter  = 0;
+                                    change_sate(cfg, on_line);
+                                }
                             }
                             else
                             {
-                                conter = 0;
+                                if(result > 0)
+                                {
+                                    conter++;
+                                }
+                                else
+                                {
+                                    conter = 0;
+                                }
+                                if(conter >= (unsigned)cfg_getint(cfg, "up_cnt"))
+                                {
+                                    on_line = true;
+                                    conter  = 0;
+                                    change_sate(cfg, on_line);
+                                }
                             }
-                            if(conter >= (unsigned)cfg_getint(cfg, "up_cnt"))
-                            {
-                                on_line = true;
-                                conter  = 0;
-                                change_sate(cfg, on_line);
-                            }
+                            usleep(cfg_getint(cfg, "interval") * 1000u);
                         }
-                        usleep(cfg_getint(cfg, "interval") * 1000u);
+                    }
+                    else
+                    {
+                        trace_log_printf(LOG_ERROR, "can't set PING_OPT_DEVICE");
                     }
                 }
                 else
                 {
-                    trace_log_printf(LOG_ERROR, "can't set PING_OPT_DEVICE");
+                    trace_log_printf(LOG_ERROR, "can't set PING_OPT_TIMEOUT");
                 }
+                ping_destroy(ping);
             }
             else
             {
-                trace_log_printf(LOG_ERROR, "can't set PING_OPT_TIMEOUT");
+                trace_log_printf(LOG_ERROR, "can't create ping object");
             }
-            ping_destroy(ping);
         }
         else
         {
-            trace_log_printf(LOG_ERROR, "can't create ping object");
+            trace_log_printf(LOG_ERROR, "invalid configuration");
         }
+
+        cfg_free(cfg);
     }
     else
     {
-        trace_log_printf(LOG_ERROR, "invalid configuration");
+        trace_log_printf(LOG_ERROR, "usage: %s <config_file>", argv[0]);
     }
-
-    cfg_free(cfg);
 
     trace_log_printf(LOG_INFO, "application stopped");
 
